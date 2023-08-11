fbe9d76c402c31cd5bd0b386c21af986254f7d76
Ashok Kumar <ashok2.kumar@amd.com>
2023-08-11 08:52:28 -0500

    bmc-ppr: Made change to return Array instead of one item
    Made change to return Array instead of one by one item
    
    Signed-off-by: Ashok Kumar <ashok2.kumar@amd.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,65 @@
 #include "ppr.hpp"
+#include "boot.hpp"
+
+void CreatePprDir()
+{
+        int dir;
+        struct stat buffer;
+
+        if (stat(kPprDir.data(), &buffer) != 0) {
+                //PPR Dir does not exist, create it
+                dir = mkdir(kPprDir.data(), 0777);
+                if(dir != 0) {
+                        sd_journal_print(LOG_ERR, "PPR directory not created\n");
+                }
+                else {
+                        sd_journal_print(LOG_ERR, "New PPR directory was created\n");
+                }
+        }
+}
 
 int main() {
+
+	//event
+	BootTimePprDataHolder* bootTimePprDataHolderObj =
+			bootTimePprDataHolderObj->getInstance();
 
 	int ret = 0;
 
 	sd_event *event = nullptr;
+
+	CreatePprDir();
+
 	ret = sd_event_default(&event);
 	if (ret < 0) {
 		sd_journal_print(LOG_ERR, "Error creating a default sd_event handler");
 		return ret;
 	}
 
-	EventPtr eventP { event };
+	EventPtr eventQ { event };
+	EventPtr1 eventP { event };
 	event = nullptr;
+
 
 	sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
 	sdbusplus::server::manager_t m { bus, DBUS_OBJECT_NAME };
 
 	bus.request_name(DBUS_SERVICE_NAME);
 
-	childPprData PprData { bus, DBUS_OBJECT_NAME, eventP };
+	childPprData PprData { bus, DBUS_OBJECT_NAME, eventQ };
+	BootTimePprData IodeviceInfo{bus, DBUS_OBJECT_NAME, eventP};
 
 	sd_journal_print(LOG_DEBUG, "Created PPR Data Object \n");
 
 	try {
 		bus.attach_event(eventP.get(), SD_EVENT_PRIORITY_NORMAL);
 		ret = sd_event_loop(eventP.get());
+		if (ret < 0) {
+			sd_journal_print(LOG_ERR,
+					"Error occurred during the sd_event_loop, RET=%d", ret);
+		}
+		bus.attach_event(eventQ.get(), SD_EVENT_PRIORITY_NORMAL);
+		ret = sd_event_loop(eventQ.get());
 		if (ret < 0) {
 			sd_journal_print(LOG_ERR,
 					"Error occurred during the sd_event_loop, RET=%d", ret);
