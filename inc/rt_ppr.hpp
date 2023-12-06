97da5cd3a3194d652b087211ad7fe657578cebd8
Mohsen Dolaty <mohsen.dolaty@amd.com>
2023-12-06 12:48:45 -0600

    bmc-ppr:add Boot Time PPR
    Add Boot Time PPR src code to PPR ap as bt_ppr.
    Rename old ppr files to runtime PPR (rt_ppr).
    Create a thread to listen for the door bell on BMC Que2 before starting
    BT PPR.
    
    Signed-off-by: Mohsen Dolaty <mohsen.dolaty@amd.com>

---
+++ inc/rt_ppr.hpp
@@ -0,0 +1,265 @@
+/*
+ // Copyright (c) 2023 AMD Inc.
+ //
+ // Licensed under the Apache License, Version 2.0 (the "License");
+ // you may not use this file except in compliance with the License.
+ // You may obtain a copy of the License at
+ //
+ //      http://www.apache.org/licenses/LICENSE-2.0
+ //
+ // Unless required by applicable law or agreed to in writing, software
+ // distributed under the License is distributed on an "AS IS" BASIS,
+ // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ // See the License for the specific language governing permissions and
+ // limitations under the License.
+ */
+#pragma once
+#include <config.h>
+
+#include <iostream>
+#include <tuple> // for tuple
+#include <vector>
+
+// Library effective with Linux
+#include <ctype.h>
+#include <systemd/sd-journal.h>
+#include <unistd.h>
+
+#include <experimental/filesystem>
+#include <filesystem>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <regex>
+#include <string_view>
+#include <utility>
+#include <xyz/openbmc_project/Collection/DeleteAll/server.hpp>
+#include <xyz/openbmc_project/Common/error.hpp>
+#include <xyz/openbmc_project/PostPackageRepair/PprData/server.hpp>
+
+extern "C" {
+#include <sys/stat.h>
+
+#include "apml.h"
+#include "esmi_cpuid_msr.h"
+#include "esmi_mailbox.h"
+#include "esmi_mailbox_nda.h"
+#include "esmi_rmi.h"
+#include "i2c/smbus.h"
+#include "linux/i2c-dev.h"
+}
+
+constexpr std::string_view kPprDir = "/var/lib/amd-ppr/";
+
+const int MAX_RETRIES = 10;
+const int RAS_ACTION_ID_RUNTIME_PPR = 0;
+const int PAYLOAD_SIZE = 10;
+const int MAX_REPAIR_SLOTS = 64;
+const int MAX_CURRENT_Runtime_PPR = 8;
+
+inline std::string getPprRuntimeFilename(int num)
+{
+    return "pprRuntime" + std::to_string(num) + ".json";
+}
+
+inline std::string getPprBoottimeFilename(int num)
+{
+    return "pprBoottime" + std::to_string(num) + ".json";
+}
+
+// PPR Service
+
+const static constexpr char* pprDataInPath =
+    "/xyz/openbmc_project/PostPackageRepair/PprData";
+const static constexpr char* PropertiesIntf = "org.freedesktop.DBus.Properties";
+
+#define PPR_TYPE_RUNTIME_MASK (0x00)
+#define PPR_TYPE_BOOTTIME_MASK (0x8000)
+#define PPR_TYPE_SOFT_MASK (0x00)
+#define PPR_TYPE_HARD_MASK (0x01)
+#define PPR_TYPE_MBIST_MASK (0x03)
+
+enum REPAIRTYPE
+{
+    PPR_TYPE_RUNTIME_SOFT = (PPR_TYPE_RUNTIME_MASK | PPR_TYPE_SOFT_MASK),
+    PPR_TYPE_RUNTIME_HARD = (PPR_TYPE_RUNTIME_MASK | PPR_TYPE_HARD_MASK),
+    PPR_TYPE_RUNTIME_MBIST = (PPR_TYPE_RUNTIME_MASK | PPR_TYPE_MBIST_MASK),
+    PPR_TYPE_BOOTTIME_SOFT = (PPR_TYPE_BOOTTIME_MASK | PPR_TYPE_SOFT_MASK),
+    PPR_TYPE_BOOTTIME_HARD = (PPR_TYPE_BOOTTIME_MASK | PPR_TYPE_HARD_MASK),
+    PPR_TYPE_BOOTTIME_MBIST = (PPR_TYPE_BOOTTIME_MASK | PPR_TYPE_MBIST_MASK),
+};
+
+// cmd 0x67 Get PPR RAS Action Status
+// Repair Status
+enum PPR_STATUS
+{
+    PPR_STATUS_REPAIR_FAIL = 0x0,
+    PPR_STATUS_REPAIR_PASS = 0x1,
+    PPR_STATUS_REPAIR_RANK_MISS_MATCH_ERROR = 0x2,
+    PPR_STATUS_REPAIR_RANK_INVALID_ERROR = 0x4,
+    PPR_STATUS_REPAIR_BANK_INVALID_ERROR = 0x08,
+    PPR_STATUS_REPAIR_SOCKET_INVALID_ERROR = 0x10,
+    PPR_STATUS_REPAIR_CHANNEL_INVALID_ERROR = 0x20,
+    PPR_STATUS_REPAIR_DEVICE_INVALID_ERROR = 0x40,
+    PPR_STATUS_REPAIR_DEVICE_MISMATCH_ERROR = 0x80,
+    PPR_STATUS_REPAIR_NOT_PROCESSED = 256,
+};
+
+struct PPR_Data
+{
+    uint16_t repairEntryNum;
+    uint16_t repairType;
+    uint16_t socNum;
+    uint16_t repairResult;
+    uint16_t payload[PAYLOAD_SIZE];
+};
+
+struct EventDeleter
+{
+    void operator()(sd_event* event) const
+    {
+        event = sd_event_unref(event);
+    }
+};
+
+using EventPtr = std::unique_ptr<sd_event, EventDeleter>;
+
+using repairtype_t = uint16_t;
+using repairentrynum_t = uint16_t;
+using socnum_t = uint16_t;
+using repairresult_t = uint16_t;
+
+using ppr_data =
+    sdbusplus::xyz::openbmc_project::PostPackageRepair::server::PprData;
+using delete_all =
+    sdbusplus::xyz::openbmc_project::Collection::server::DeleteAll;
+
+class commonPPR
+{
+    static commonPPR* instance;
+    commonPPR()
+    {
+    }
+
+  public:
+    static commonPPR* getInstance()
+    {
+        if (!instance)
+            instance = new commonPPR;
+        return instance;
+    }
+
+    uint16_t getBTindex()
+    {
+        return m_pprBoottimeIndex;
+    }
+
+    void setBTindex(uint16_t index)
+    {
+        m_pprBoottimeIndex = index;
+    }
+
+    std::tuple<uint16_t, uint16_t, uint16_t, uint16_t, std::vector<uint16_t>>
+        getBTdata(uint16_t index)
+    {
+        std::tuple<uint16_t, uint16_t, uint16_t, uint16_t,
+                   std::vector<uint16_t>>
+            tup;
+
+        if (index < MAX_REPAIR_SLOTS)
+        {
+            std::vector<uint16_t> vec;
+            for (int i = 0; i < PAYLOAD_SIZE; i++)
+                vec.push_back(m_pprBoottimeData[index].payload[i]);
+            tup = std::make_tuple(m_pprBoottimeData[index].repairEntryNum,
+                                  m_pprBoottimeData[index].repairType,
+                                  m_pprBoottimeData[index].socNum,
+                                  m_pprBoottimeData[index].repairResult, vec);
+        }
+        return tup;
+    }
+
+    void setBTdata(uint16_t repairEntryNum, uint16_t repairType,
+                   uint16_t socNum, uint16_t result,
+                   std::vector<uint16_t> payload)
+    {
+        m_pprBoottimeData[m_pprBoottimeIndex].repairEntryNum = repairEntryNum;
+        m_pprBoottimeData[m_pprBoottimeIndex].repairType = repairType;
+        m_pprBoottimeData[m_pprBoottimeIndex].socNum = socNum;
+        m_pprBoottimeData[m_pprBoottimeIndex].repairResult = result;
+        for (int j = 0; j < PAYLOAD_SIZE; j++)
+            m_pprBoottimeData[m_pprBoottimeIndex].payload[j] = payload[j];
+        m_pprBoottimeIndex++;
+    }
+
+    void setBTresult(uint16_t index, uint16_t result)
+    {
+        m_pprBoottimeData[index].repairResult = result;
+    }
+
+  private:
+    uint16_t m_pprBoottimeIndex;
+    std::array<PPR_Data, MAX_REPAIR_SLOTS> m_pprBoottimeData;
+};
+
+// PPR Class
+struct childPprData : sdbusplus::server::object_t<ppr_data, delete_all>
+{
+
+    commonPPR* globalBT = globalBT->getInstance();
+
+    childPprData(sdbusplus::bus::bus& bus, const char* path, EventPtr& event) :
+        sdbusplus::server::object_t<ppr_data, delete_all>(bus, path), bus(bus),
+        event(event)
+    {
+
+        m_pprRuntimeIndex = 0;
+        m_currentRuntimeIndex = 0;
+        m_currentRuntimeCnt = 0;
+        globalBT->setBTindex(0);
+        sd_journal_print(LOG_ERR, "PPR Data Constructor - Check \n");
+        updateBTfromBoottimeRepair();
+        updateBTfromRuntimeRepair();
+    }
+
+    ~childPprData()
+    {
+    }
+
+    void deleteAll() override;
+    // Set values of repair data
+    bool setPostPackageRepairData(uint16_t repairEntryNum, uint16_t repairType,
+                                  uint16_t socNum,
+                                  std::vector<uint16_t> payload) override;
+
+    // Start run time post package repair
+    uint32_t startRuntimeRepair(uint16_t repairSlot) override;
+
+    // Get status of repair
+    std::vector<std::tuple<uint16_t, uint16_t, uint16_t, uint16_t,
+                           std::vector<uint16_t>>>
+        getPostPackageRepairStatus() override;
+
+    // Set value of RecordAd
+    virtual bool recordAdd(bool value) override;
+    void UpdatePprResult(int index, uint16_t repairResult, uint16_t repairType);
+    void WritePprFile(int index, uint16_t repairEntryNum, uint16_t repairType,
+                      uint16_t socNum, std::vector<uint16_t> payload);
+    uint16_t GetRuntimeIndex(void);
+    std::tuple<uint16_t, uint16_t, uint16_t, uint16_t, std::vector<uint16_t>>
+        getRuntimeData(uint16_t index, uint16_t slot);
+    std::tuple<uint16_t, uint16_t, uint16_t, uint16_t, std::vector<uint16_t>>
+        getBoottimeData(uint16_t index);
+
+  private:
+    sdbusplus::bus::bus& bus;
+    EventPtr& event;
+
+    std::array<PPR_Data, MAX_REPAIR_SLOTS> m_pprRuntimeData;
+
+    void updateBTfromBoottimeRepair();
+    void updateBTfromRuntimeRepair();
+    uint32_t updateRuntimeRepairStatus(uint16_t index, uint16_t slot);
+    uint16_t m_pprRuntimeIndex;
+    uint16_t m_currentRuntimeIndex;
+    uint16_t m_currentRuntimeCnt;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,125 @@
-#include "ppr.hpp"
+#include "bt_ppr.hpp"
+#include "rt_ppr.hpp"
+
+#include <thread>
+
+static boost::asio::io_service io;
+std::shared_ptr<sdbusplus::asio::connection> conn;
 
 void CreatePprDir()
 {
-        int dir;
-        struct stat buffer;
+    int dir;
+    struct stat buffer;
 
-        if (stat(kPprDir.data(), &buffer) != 0) {
-                //PPR Dir does not exist, create it
-                dir = mkdir(kPprDir.data(), 0777);
-                if(dir != 0) {
-                        sd_journal_print(LOG_ERR, "PPR directory not created\n");
-                }
-                else {
-                        sd_journal_print(LOG_ERR, "New PPR directory was created\n");
-                }
+    if (stat(kPprDir.data(), &buffer) != 0)
+    {
+        // PPR Dir does not exist, create it
+        dir = mkdir(kPprDir.data(), 0777);
+        if (dir != 0)
+        {
+            sd_journal_print(LOG_ERR, "PPR directory not created\n");
         }
-}
-int main() {
-
-	int ret = 0;
-
-	sd_event *event = nullptr;
-
-	CreatePprDir();
-
-	ret = sd_event_default(&event);
-	if (ret < 0) {
-		sd_journal_print(LOG_ERR, "Error creating a default sd_event handler");
-		return ret;
-	}
-
-	EventPtr eventP { event };
-	event = nullptr;
-
-	sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
-	sdbusplus::server::manager_t m { bus, DBUS_OBJECT_NAME };
-
-	bus.request_name(DBUS_SERVICE_NAME);
-
-	childPprData PprData { bus, DBUS_OBJECT_NAME, eventP };
-
-	sd_journal_print(LOG_DEBUG, "Created PPR Data Object \n");
-
-	try {
-		bus.attach_event(eventP.get(), SD_EVENT_PRIORITY_NORMAL);
-		ret = sd_event_loop(eventP.get());
-		if (ret < 0) {
-			sd_journal_print(LOG_ERR,
-					"Error occurred during the sd_event_loop, RET=%d", ret);
-		}
-	} catch (std::exception &e) {
-		sd_journal_print(LOG_ERR, "Error:%s", e.what());
-		return -1;
-	}
-	return 0;
-
+        else
+        {
+            sd_journal_print(LOG_ERR, "New PPR directory was created\n");
+        }
+    }
 }
 
+void ReadHostQueue2()
+{
+    int fd = -1;
+    char buff[Q2_READ_CNT];
+    ssize_t ret;
+    BootTimePprData* btPpr = NULL;
+
+    while (1)
+    {
+        sd_journal_print(LOG_INFO, " ReadHostQueue2: Start \n");
+        fd = open(index_file, O_RDWR, 0);
+        if (fd < 0)
+        {
+            sd_journal_print(LOG_ERR,
+                             " ReadHostQueue2: Failed to Open Que 2\n");
+            close(fd);
+            exit(-1);
+        }
+        ret = read(fd, buff, Q2_READ_CNT);
+        if (ret < 0)
+        {
+            sd_journal_print(
+                LOG_ERR, " ReadHostQueue2: Read Failed with return %d\n", ret);
+        }
+        else
+        {
+            sd_journal_print(LOG_INFO,
+                             " ReadHostQueue2:Read return (%d) Data 0x%x "
+                             "0x%x 0x%x 0x%x \n",
+                             ret, buff[0], buff[1], buff[2], buff[3]);
+            if ((buff[0] == Q2_BIOS_SIG) && (buff[1] == Q2_BIOS_SIG) &&
+                (buff[2] == Q2_BIOS_SIG) && (buff[3] == Q2_BIOS_SIG))
+            {
+                if (btPpr != NULL)
+                    delete btPpr;
+                btPpr = new BootTimePprData();
+            }
+        }
+        close(fd);
+        sleep(1);
+    } // end of while
+}
+
+int main()
+{
+    // connect to dbus
+    conn = std::make_shared<sdbusplus::asio::connection>(io);
+
+    commonPPR* globalBT = globalBT->getInstance();
+
+    int ret = 0;
+
+    sd_event* event = nullptr;
+
+    CreatePprDir();
+
+    ret = sd_event_default(&event);
+    if (ret < 0)
+    {
+        sd_journal_print(LOG_ERR, "Error creating a default sd_event handler");
+        return ret;
+    }
+
+    // events
+    EventPtr eventP{event};
+    event = nullptr;
+
+    sdbusplus::bus::bus bus = sdbusplus::bus::new_default();
+    sdbusplus::server::manager_t m{bus, DBUS_OBJECT_NAME};
+
+    bus.request_name(DBUS_SERVICE_NAME);
+
+    childPprData RtPprData{bus, DBUS_OBJECT_NAME, eventP};
+
+    sd_journal_print(LOG_DEBUG, "Created PPR Data Object \n");
+
+    std::thread h2bQ1Thread(ReadHostQueue2);
+    h2bQ1Thread.detach();
+
+    try
+    {
+        bus.attach_event(eventP.get(), SD_EVENT_PRIORITY_NORMAL);
+        ret = sd_event_loop(eventP.get());
+        if (ret < 0)
+        {
+            sd_journal_print(LOG_ERR,
+                             "Error occurred during the sd_event_loop, RET=%d",
+                             ret);
+        }
+    }
+    catch (std::exception& e)
+    {
+        sd_journal_print(LOG_ERR, "Error:%s", e.what());
+        return -1;
+    }
+    return 0;
+}
